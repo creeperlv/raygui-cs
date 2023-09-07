77a2745d6cd162566de5e4aaba6a12e3e73a3360
Creeper Lv <creeperlv@users.noreply.github.com>
2023-09-08 07:37:19 +0800

    Add project files.

---
+++ Raygui_cs.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using raygui_cs;
+using Raylib_cs;
+
+namespace Raygui_cs.Test
+{
+    internal class Program
+    {
+        static void Main(string [ ] args)
+        {
+            Raylib.InitWindow(800 , 600 , "Raygui Test");
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+                Raylib.DrawText("RAYGUI TEST, 你好",400,280,20, Color.BLACK);
+                Raygui.GuiLabel(new Rectangle(100,100,100,100),"Test");
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
